ffc0fe17883d9bf9b249486ae6119cdc4004e786
Deepak Thayyil <deepak@ejeevalive.onmicrosoft.com>
2020-04-27 13:43:26 +0530

    Publish to docs folder for github pages

---
+++ docs/precache-manifest.281057e61608836d23d8932c533d8b68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18e62ead0fc88405adcb",
+    "url": "/static/css/main.1a4632bb.chunk.css"
+  },
+  {
+    "revision": "18e62ead0fc88405adcb",
+    "url": "/static/js/main.18e62ead.chunk.js"
+  },
+  {
+    "revision": "62cebc8120ba30530542",
+    "url": "/static/js/1.62cebc81.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5c6e625df7be3779f6e211c8b7b7d0e6",
+    "url": "/index.html"
+  }
+];
